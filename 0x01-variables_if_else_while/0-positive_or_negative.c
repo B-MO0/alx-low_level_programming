5194ffd0b34eff501ffeb6421ceb7a9deac0e1af
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-06 13:37:17 -0700

    ll

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if(n==0)
 	{
-	printf("%d \: is zero");
+	printf("%d is zero\n");
 	} else if(n<0)
 	{
-	printf("%d \: is negative");
+	printf("%d is negative"\n);
 	}
 	else{
-	printf("%d \: is positive");
+	printf("%d is positive\n");
 	}
 	return (0);
 }
