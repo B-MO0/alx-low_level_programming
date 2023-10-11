f85ffae6f291517e86996fbb35659384bbb17a31
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-11 15:57:21 -0700

    kk

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,12 @@
 #include "main.h"
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
 void print_times_table(int n)
 {
 int i, j, k;
 if (n >= 0 && n <= 15)
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i <= n)
 {
-for (j = 0; j <= n; j++)
+for (j = 0; j <= n)
 {
 k = j * i;
 if (j == 0)
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-/**
-* print_to_98 - prints all natural numbers from n to 98,
-* followed by a new line
-* @n: print from this number
-*/
 void print_to_98(int n)
 {
 int i, j;
 if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+for (i = n; i <= 98)
 {
 if (i != 98)
 printf("%d, ", i);
 else if (i == 98)
 printf("%d\n", i);
 }
-}
-else if (n >= 98)
+} else if (n >= 98)
 {
-for (j = n; j >= 98; j--)
+for (j = n; j >= 98)
 {
 if (j != 98)
 printf("%d, ", j);
