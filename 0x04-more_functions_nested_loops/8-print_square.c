bb7d09c236c863a4e770395bb99372ffc33e056c
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-12 15:02:15 -0700

    hh

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,49 +1,13 @@
 #include "main.h"
 
 /**
- * print_square - prints a square, followed by a new line;
- * @size: size of the square
+ * main - check the code
+ * Return: Always 0.
  */
-void print_square(int size)
+int main(void)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
-
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-#include "main.h"
-
-/**
- * print_square - prints a square, followed by a new line;
- * @size: size of the square
- */
-void print_square(int size)
-{
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
-
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-}}
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
+}
