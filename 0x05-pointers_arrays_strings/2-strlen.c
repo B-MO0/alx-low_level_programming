67b0fc52ed2cebd3ddb8e812564d5361d596b634
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-16 13:49:41 -0700

    hh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#inlucde "main.h"
+/**
+* _strlen - lenght of string
+*@s:string
+*Return:length
+*/
+int _strlen(char *s);
+{
+int length = 0;
+while(*s != '\0')
+{
+length++;
+s++;
+}
+return(length);
+}
