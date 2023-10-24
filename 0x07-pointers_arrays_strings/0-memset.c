83710d6f8bc3a60b0e261ac6dada14c2dc62ffc5
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-24 13:37:20 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ *_memset- main proto
+ *
+ *return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int = a
+  for (a = 0; a < n; a++)
+    s[a] = b;
+return (s);
+}
