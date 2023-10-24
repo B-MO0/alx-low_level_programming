bf2de158c25cffe73efbebb7066dd08ee788e5b6
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-24 13:57:33 -0700

    t

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
-*_memset - main proto
-*@n: Function parap
-*@s: Function param
-*@b: memory value
-*
-*Return: s
-*/
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the memory area to be filled
+ * @b: The byte to be set
+ * @n: The number of bytes to be filled
+ *
+ * Return: Pointer to the filled memory area (s)
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
-for (a = 0; a < n; a++)
-s[a] = b;
-return (s);
+    unsigned int a;
+    
+    for (a = 0; a < n; a++)
+        s[a] = b;
+    
+    return (s);
 }
