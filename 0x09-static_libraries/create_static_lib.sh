bce981d2290e0b6fed27fddda7211732d375c9bd
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-28 10:07:04 -0700

    ff

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,17 +1,30 @@
-c_files=()
+source_files=(
+    "0-isupper.c"
+    "0-strcat.c"
+    "1-isdigit.c"
+    "1-strncat.c"
+    "2-strlen.c"
+    "3-islower.c"
+    "3-strcmp.c"
+    "4-isalpha.c"
+    "5-strstr.c"
+    "9-strcpy.c"
+    "_putchar.c"
+    "0-memset.c"
+    "100-atoi.c"
+    "1-memcpy.c"
+    "2-strchr.c"
+    "2-strncpy.c"
+    "3-puts.c"
+    "3-strspn.c"
+    "4-strpbrk.c"
+    "6-abs.c"
+)
 
-while IFS= read -r -d $'\0' file; do
-  c_files+=("$file")
-done < <(find "$(pwd)" -type f -name "*.c" -print0)
-
-for c_file in "${c_files[@]}"; do
-  gcc -c "$c_file"
+for file in "${source_files[@]}"; do
+    gcc -c "$file"
 done
 
-o_files=()
-
-while IFS= read -r -d $'\0' file; do
-  o_files+=("$file")
-done < <(find "$(pwd)" -type f -name "*.o" -print0)
+o_files=(*.o)
 
 ar rc liball.a "${o_files[@]}"
