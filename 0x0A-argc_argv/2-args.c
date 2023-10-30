f5911e7d373786fca6ee02901c08f7b016c4b12c
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-30 11:31:27 -0700

    gg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print argument values in new lines
+*@argc:int
+*@argv:list
+*@i: just a number
+*Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+int i = 0;
+while (argc--)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+}
