f28006a9f02a7251957f4998a20af8ea2ff0549e
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-30 11:09:44 -0700

    hh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - program prints number of arguments passed to it witout counting the first element
+ *@argc:int
+ *@argv:list
+ *Return: always 0
+*/
+int main(int argc, char const *argv[])
+{
+  void(argv);
+  printf("%d\n", argc - 1);
+  return (0);
+}
