2da4408ac39debec709accd640c84f262ff4bcaf
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-30 11:56:17 -0700

    gg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,12 +4,12 @@
 *main - multipy 2 nums
 *@argc: int
 *@argv: list
-*Return : always 0
+*Return: always 0
 */
 int main(int argc, char *argv[])
 {
 int mult;
-if (argc == 3)
+if(argc == 3)
 {
 mult = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", mult);
