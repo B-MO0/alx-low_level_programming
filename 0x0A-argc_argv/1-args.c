606fb063724a82784a5eea4f9391efa05416f0a5
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-30 11:17:10 -0700

    gg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,9 @@
  *@argv:list
  *Return: always 0
 */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-  void(argv);
+  void(*argv[]);
   printf("%d\n", argc - 1);
   return (0);
 }
