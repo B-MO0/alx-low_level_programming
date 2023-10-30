37ddeb4f789bc68d3f666b28c0b92c9a7eceb8d2
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-10-30 11:20:53 -0700

    gg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - program prints number of arguments passed to it witout counting the first element
+ *main - program prints number of arguments without the first
  *@argc:int
  *@argv:list
  *Return: always 0
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
   printf("%d\n", argc - 1);
   return (0);
