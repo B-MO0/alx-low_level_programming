accdb6732a85359743c9a5e38c0205287f75d864
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-01 14:00:31 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-*main - sraifa
-*@argc: number of arguments
-*@argv: list of arguments
-*Return: always 0
+* main - Calculate the minimum number of coins needed for change
+* @argc: number of arguments
+* @argv: list of arguments
+* Return: 0 for success, 1 for error
 */
 int main(int argc, char *argv[])
 {
 if (argc == 2)
 {
-int i, leastcents = 0, money = atoi (argv[1]);
+int i, leastcents = 0, money = atoi(argv[1]);
 int cents[] = {25, 10, 5, 2, 1};
+
 for (i = 0; i < 5; i++)
 {
-if (money >= cents [i])
+if (money >= cents[i])
 {
 leastcents += money / cents[i];
 money = money % cents[i];
+
 if (money % cents[i] == 0)
 {
 break;
@@ -31,5 +34,6 @@ else
 printf("Error\n");
 return (1);
 }
-return (0);
+
+return (0); 
 }
