8417bff38d0ee66ff6f97d387803a4409f0fcdcf
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-01 13:34:03 -0700

    ff

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*main - adds up the values of list
-*@argc: int
-*@argv: list
-*Return: always 0
+*main - adds up the values in the list
+*@argc: number of arguments
+*@argv: list of arguments
+*Return: 0 for success, 1 for error
 */
 int main(int argc, char *argv[])
 {
 int sum = 0;
 char *c;
-while (argc)
+while (argc > 1) 
+for (c = argv[argc - 1]; *c; c++)
 {
-for (c = argv[argc]; *c; c++)
-if (*c < '0' || *c >'9')
-prinf("Error\n");
- return (1);
-sum += atoi(argv[argc]);
+if (*c < '0' || *c > '9')
+{
+printf("Error\n");
+return (1); // Error
+}
+}
+sum += atoi(argv[argc - 1]);
+argc--;
 }
 printf("%d\n", sum);
 return (0);
