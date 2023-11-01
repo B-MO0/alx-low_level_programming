55f0721ace2afe33678c38d34993f8c0afb6b10c
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-01 15:10:12 -0700

    gg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,55 @@
 #include "main.h"
-/**
-*_strlen - find length of a string
-*@s: string
-*Return: int
-*/
+#include <stdlib.h>
 
+/**
+* _strlen - find the length of a string
+* @s: string
+* Return: integer
+*/
 int _strlen(char *s)
 {
-int size = O;
-for (; s[size] != '\0'; size++
-;
+int size = 0;
+for (; s[size] != '\0'; size++)
+{
+
+}
 return (size);
 }
+
 /**
-**str_concat - concatenates two strings
-*@sl: string 1
-*@s2: string 2
-*Return: pointer
+* str_concat - concatenates two strings
+* @s1: string 1
+* @s2: string 2
+* Return: pointer to the concatenated string
 */
-char *str_concat(char *sl, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 int size1, size2, i;
 char *m;
+
 if (s1 == NULL)
-s1 = "\0";
+s1 = "";
 if (s2 == NULL)
-s2 = "\0";
+s2 = "";
+
 size1 = _strlen(s1);
-size1 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) +1);
-return (0);
-for (i = 0; i <= size1 + size2; i++)
+size2 = _strlen(s2);
+
+m = malloc((size1 + size2 + 1) * sizeof(char));
+
+if (m == NULL)
+return (NULL);
+
+for (i = 0; i < size1; i++)
 {
-if (i < size1)
 m[i] = s1[i];
-else
+}
+
+for (; i < size1 + size2; i++)
+{
 m[i] = s2[i - size1];
 }
-m[i] = '\0'
+
+m[i] = '\0';
 return (m);
 }
