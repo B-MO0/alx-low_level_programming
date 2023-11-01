3f9abcc93ca554169a7bd2fff8aca7e7db6a4327
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-01 14:52:31 -0700

    typo

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ if (str == NULL)
 return (NULL);
 for (; str[size] != '\0'; size++)
 ;
-m = malloc(size * sizeof(*stf) + 1);
+m = malloc(size * sizeof(*str) + 1);
 if (m == 0)
 return (NULL);
 else
