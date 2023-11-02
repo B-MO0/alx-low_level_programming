0d1b642cf36e09b48ef391e729e3d812a9922c14
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-02 13:30:40 -0700

    first task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+**malloc_checked - allocate memory using malloc, exit if error 98
+*@b: int
+*Return: pointer to the array initialized or NULL.
+*/
+void *malloc_checked(unsigned int b);
+{
+int *m = malloc(b);
+if (m==0)
+exit(98);
+return (m)
+}
