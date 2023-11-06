ba8b70bfb7b42a8d0a306d3147baacbc12829b18
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-06 05:34:54 -0800

    gg

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*init_dog - initialize the dog
+*@d: struct ptr
+*@name: dogo name
+*@age: dogo age
+*@owner: dogo dogo
+*
+*Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d->name = name;
+d->owner = owner;
+d->age = age;
+}
+}
