6e05e06fb22e469fa7602e91bee40265b4dfb972
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-06 09:39:00 -0800

    done!!

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - freedom for the dags
+*@d: ptr structure dog
+*
+*Return: void
+*/
+void free_dog(dog_t *d)
+{
+if (d)
+{
+if (d->name)
+free(d->name);
+if (d->age)
+free(d->age);
+if (d->owner)
+free(d->owner);
+free (d);
+}
+}
