7e5b2778448c45e037f93f5a991e38f3359a6d4b
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-06 08:38:33 -0800

    ff

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
-* print_dog print nil if element is null
+* print_dog - print nil if element is null
 *@d: pointer to struct
 *
 *Return: void
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 if (d)
 {
 printf("Name: %s\n", d->name ? d->name : "(nil)");
-printf("Age: %f\n", d->age ? d->age : "(nil)");
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
