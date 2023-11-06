43ba300eeb8fd0d87a4a66929d69b73f880ab808
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-06 05:51:45 -0800

    hh

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*
+*/
+void print_dog(struct dog *d)
+{
+  if (d)
+    {
+      printf("Name: %s\n", d->name ? d->name : "(nil)");
+      printf("Age: %s\n", d->age ? d->age : "(nil)");
+      printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+    }
+}
