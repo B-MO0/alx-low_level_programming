ba85c0cea8bff3a558a8b8df9c0785df675cfd24
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-06 08:44:50 -0800

    gg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,16 @@
 *
 *Description: dog's info
 */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
-
-typedef struct dog dog_t;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
 
