a2d92edb6529710f0dfaa9b1daea1d06e2193a67
B-MO0 <contact.mohamedballouchi@gmail.com>
2023-11-08 13:41:08 -0800

    gg

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
 *
 *Return: void
 */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int *end = array + size - 1;
 if (array && size && action)
