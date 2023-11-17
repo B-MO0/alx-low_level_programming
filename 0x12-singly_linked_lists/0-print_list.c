a131ee7034469b0bca9ade2692e7382ca1f0d107
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2023-11-17 13:21:56 -0800

    here we go

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "list.h"
+/**
+*_strlen - returns length of string
+*@s: just a string
+*
+*Return: string length
+*/
+int _strlen(char *s)
+{
+int i = 0;
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
+}
+/**
+ *
+ */
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h)
+}
+printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+h = h->next;
+i++;
+}
+return (i);
+}
