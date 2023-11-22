ab49adbe2b9ea16091a627b91eb812a8519c53e3
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2023-11-22 23:08:14 +0100

    hh

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
-
 /**
 * get_nodeint_at_index - returns the nth node of a listint_t linked list.
 * @head: pointer 
-* @index:index of the node
+* @index: index of the node
 *
 * Return: pointer of Index
 */
