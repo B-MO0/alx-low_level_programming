8befcbf7504af8cf79c6f86d40d5b750bf450111
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2023-11-24 14:02:55 -0800

    g

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* get_endianness - checks the endianness
+*
+* Return: 0 , or 1 
+*/
+int get_endianness(void)
+{
+int i;
+char *c;
+i = 1;
+c = (char *)&i;
+return (*c);
+}
