376e7ae285d231caa3c604899ed5326804b08b7d
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2023-12-15 22:08:10 -0800

    GG

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,29 @@
 #include "main.h"
+
 /**
-* get_endianness - checks the endianness
+* binary_to_uint -converts a binary number to an unsigned int.
+* @b: pointing to a string of 0 and 1 chars
 *
-* Return: 0 if big endian, 1 if little endian
+* Return:converted number, or 0
 */
-int get_endianness(void)
+unsigned int binary_to_uint(const char *b)
 {
 int i;
-char *c;
+unsigned int j;
 
-i = 1;
-c = (char *)&i;
-
-return (*c);
+j = 0;
+if (!b)
+return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+}
+for (i = 0; b[i] != '\0'; i++)
+{
+j <<= 1;
+if (b[i] == '1')
+j += 1;
+}
+return (j);
 }
