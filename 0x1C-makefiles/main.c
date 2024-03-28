58fff08dc94bec07882f6f2a3da38a8f45f4db59
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2024-03-28 11:37:11 +0000

    makefile

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "mk.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	print_seahorse();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x1C-makefiles/mk.h
@@ -0,0 +1,9 @@
+#ifndef __MK_H__
+#define __MK_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_seahorse(void);
+
+#endif
